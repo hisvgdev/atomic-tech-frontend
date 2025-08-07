df1f130062e37f9f63e96de8474e70b1a10681b3
hisvgdev <kozhemyakokirill@gmail.com>
2025-08-08 02:56:03 +0300

    fix: 🐛 error

--- app/(banner)/layout.tsx
+++ app/(banner)/layout.tsx
@@ -2,11 +2,11 @@ import BannerWithTags from '@/shared/global/BannerWithTags'
 import React from 'react'
 
 export default function Layout({ children }: Readonly<{ children: React.ReactNode }>) {
-    return (
-        <div className="flex flex-col gap-y-10 px-4 lg:px-7">
-            <BannerWithTags />
-            <hr />
-            {children}
-        </div>
-    )
+     return (
+          <div className="flex flex-col gap-y-10 px-4 lg:px-7">
+               <BannerWithTags />
+               <hr />
+               {children}
+          </div>
+     )
 }
--- components/articles/organism/Grid.tsx
+++ components/articles/organism/Grid.tsx
@@ -1,9 +1,9 @@
 import ArticlesCards from '../molecules/ArticlesCards'
 
 export const Grid = () => {
-    return (
-        <div className="flex flex-col gap-10">
-            <ArticlesCards />
-        </div>
-    )
+     return (
+          <div className="flex flex-col gap-10">
+               <ArticlesCards />
+          </div>
+     )
 }
