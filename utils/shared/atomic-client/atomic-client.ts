be9a8d5d3e4069db808e8ec8ef8b0e6752dd1018
hisvgdev <kozhemyakokirill@gmail.com>
2025-12-07 05:08:28 +0300

    fix: 🐛 delete slash

--- utils/shared/atomic-client/atomic-client.ts
+++ utils/shared/atomic-client/atomic-client.ts
@@ -67,7 +67,7 @@ export class AtomicClient {
      }
 
      private async fetchProxyToken(): Promise<string> {
-          const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/auth/service-token`)
+          const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}api/auth/service-token`)
           const token = await response.json()
 
           if (!token) {
